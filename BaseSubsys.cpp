0aa23683170daf898364e9f198780eaa2e02827d
papymojo <benjamin.bercy@orange.fr>
2022-11-13 11:50:45 +0100

    bens patch

--- BaseSubsys.cpp
+++
@@ -1,9 +0,0 @@
-#include "BaseSubsys.h"
-
-BaseSubsys::BaseSubsys()
-{
-}
-
-Opcode BaseSubsys::lastOpcode = NO_OP;
-word BaseSubsys::ioCount = 0;
-byte BaseSubsys::ioBuffer[128];
--- Utils.h
+++
@@ -1,7 +0,0 @@
-#if !defined(_UTILS_H_)
-#define _UTILS_H_
-
-void FlushRxBuffer();
-void blinkIOSled(unsigned long *timestamp);
-
-#endif
